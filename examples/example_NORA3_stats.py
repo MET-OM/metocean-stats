a5a98c7e2c891ebca87b30afb75304beb5d49340
KonstantinChri <kchrist87@gmail.com>
2023-09-23 10:01:17 +0200

    update

--- examples/example_NORA3_stats.py
+++ examples/example_NORA3_stats.py
@@ -1,22 +1,22 @@
 from metocean_api import ts
-from metocean_engine.stats import general_stats as stats
-from metocean_engine.stats import dir_stats as dir
+from metocean_stats.stats import general_stats
+from metocean_stats.stats import dir_stats
 
 # Define TimeSeries-object
 ds = ts.TimeSeries(lon=1.320, lat=53.324,start_time='2022-11-01', end_time='2022-12-31' , product='NORA3_wind_wave')
 
 
 # Import data from thredds.met.no and save it as csv
-ds.import_ts(save_csv=True)
+ds.import_ts(save_csv=False)
 
 
-stats.scatter_diagram(var1=ds.data['hs'], step_var1=1, var2=ds.data['tp'], step_var2=2, output_file='scatter_hs_tp_diagram.png')
-#stats.table_var_sorted_by_Hs(data=ts.data, var='tp', output_file='tp_sorted_by_hs.csv')
-#stats.table_monthly_percentile(data=ts.data, var='hs', output_file='hs_monthly_perc.csv')
-#stats.table_monthly_min_mean_max(data=ts.data, var='hs',output_file='hs_montly_min_mean_max.csv')
-#dir.var_rose(ts.data, 'thq','hs','windrose.png',method='monthly')
-#dir.directional_min_mean_max(ts.data,'thq','hs','hs_dir_min_mean_max.csv')
-#dir.monthly_var_rose(ts.data,'thq','hs','windrose_month.png')
+general_stats.scatter_diagram(var1=ds.data['hs'], step_var1=1, var2=ds.data['tp'], step_var2=2, output_file='scatter_hs_tp_diagram.png')
+#general_stats.table_var_sorted_by_Hs(data=ts.data, var='tp', output_file='tp_sorted_by_hs.csv')
+#general_stats.table_monthly_percentile(data=ts.data, var='hs', output_file='hs_monthly_perc.csv')
+#general_stats.table_monthly_min_mean_max(data=ts.data, var='hs',output_file='hs_montly_min_mean_max.csv')
+#dir_stats.var_rose(ts.data, 'thq','hs','windrose.png',method='monthly')
+#dir_stats.directional_min_mean_max(ts.data,'thq','hs','hs_dir_min_mean_max.csv')
+#dir_stats.monthly_var_rose(ts.data,'thq','hs','windrose_month.png')
 
 
 
