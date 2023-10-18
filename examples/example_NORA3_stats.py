19694e4a0257ab28c11df468f5faea81f62b2455
KonstantinChri <kchrist87@gmail.com>
2023-10-18 15:06:18 +0200

    add ERA5 stats

--- examples/example_NORA3_stats.py
+++ examples/example_NORA3_stats.py
@@ -2,14 +2,13 @@ from metocean_api import ts
 from metocean_stats.stats import general_stats, dir_stats, extreme_stats
 
 # Define TimeSeries-object
-#ds = ts.TimeSeries(lon=1.32, lat=53.324,start_time='2000-01-01', end_time='2010-12-31' , product='NORA3_wind_wave')
-ds = ts.TimeSeries(lon=1.32, lat=53.324,start_time='2000-01-01', end_time='2010-12-31' , product='ERA5')
+ds = ts.TimeSeries(lon=1.32, lat=53.324,start_time='2000-01-01', end_time='2010-12-31' , product='NORA3_wind_wave')
 
 
 # Import data from thredds.met.no and save it as csv
-ds.import_data(save_csv=True)
+#ds.import_data(save_csv=True)
 # Load data from local file
-#ds.load_data('tests/data/'+ds.datafile)
+ds.load_data('tests/data/'+ds.datafile)
 
 # Generate Statistics
 #general_stats.scatter_diagram(data=ds.data, var1='hs', step_var1=1, var2='tp', step_var2=1, output_file='scatter_hs_tp_diagram.png')
@@ -18,7 +17,7 @@ ds.import_data(save_csv=True)
 #general_stats.table_monthly_min_mean_max(data=ds.data, var='hs',output_file='hs_montly_min_mean_max.csv')#
 #dir_stats.var_rose(ds.data, 'thq','hs','windrose.png',method='overall')
 #dir_stats.directional_min_mean_max(ds.data,'thq','hs','hs_dir_min_mean_max.csv')
-#rl_pot = extreme_stats.return_levels_pot(data=ds.data, var='hs', periods=[20,50,100,1000], output_file='return_levels_POT.pdf')
-#rl_am = extreme_stats.return_levels_annual_max(data=ds.data, var='hs', periods=[20,50,100,1000],method='GEV',output_file='return_levels_GEV.pdf')
+rl_pot = extreme_stats.return_levels_pot(data=ds.data, var='hs', periods=[20,50,100,1000], output_file='return_levels_POT.pdf')
+rl_am = extreme_stats.return_levels_annual_max(data=ds.data, var='hs', periods=[20,50,100,1000],method='GEV',output_file='return_levels_GEV.pdf')
 
 
