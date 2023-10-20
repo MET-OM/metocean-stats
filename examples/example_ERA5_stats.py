6f02d18bfc2070a0323d021487a6e95685bd2bb4
KonstantinChri <kchrist87@gmail.com>
2023-10-20 14:39:38 +0200

    update example_ERA5_stats

--- examples/example_ERA5_stats.py
+++ examples/example_ERA5_stats.py
@@ -12,12 +12,16 @@ ds.load_data('tests/data/'+ds.datafile)
 
 # Generate Statistics
 general_stats.scatter_diagram(data=ds.data, var1='swh', step_var1=1, var2='pp1d', step_var2=1, output_file='scatter_hs_tp_diagram.png')
-#general_stats.table_var_sorted_by_hs(data=ds.data, var='pp1d', output_file='tp_sorted_by_hs.csv')
+general_stats.table_var_sorted_by_hs(data=ds.data, var='pp1d',var_hs='swh', output_file='tp_sorted_by_hs.csv')
 general_stats.table_monthly_percentile(data=ds.data, var='swh', output_file='hs_monthly_perc.csv')
 general_stats.table_monthly_min_mean_max(data=ds.data, var='swh',output_file='hs_montly_min_mean_max.csv')#
+
+# Directional Statistics
 dir_stats.var_rose(ds.data, 'mwd','swh','windrose.png',method='overall')
 dir_stats.directional_min_mean_max(ds.data,'mwd','swh','hs_dir_min_mean_max.csv')
-rl_pot = extreme_stats.return_levels_pot(data=ds.data, var='swh', periods=[20,50,100,1000], output_file='return_levels_POT.pdf')
-rl_am = extreme_stats.return_levels_annual_max(data=ds.data, var='swh', periods=[20,50,100,1000],method='GEV',output_file='return_levels_GEV.pdf')
+
+# Extreme Statistics
+rl_pot = extreme_stats.return_levels_pot(data=ds.data, var='swh', periods=[20,50,100], output_file='return_levels_POT.png')
+rl_am = extreme_stats.return_levels_annual_max(data=ds.data, var='swh', periods=[20,50,100],method='GEV',output_file='return_levels_GEV.png')
 
 
