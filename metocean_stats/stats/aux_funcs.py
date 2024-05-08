084ffa89f7f910db97c9ef0c674c01d5f108f400
NGUYEN MANH DUNG <127861978+dung-manh-nguyen@users.noreply.github.com>
2024-05-08 14:34:09 +0200

    Update aux_funcs.py

--- metocean_stats/stats/aux_funcs.py
+++ metocean_stats/stats/aux_funcs.py
@@ -38,3 +38,50 @@ def readNora10File(file):
     df.index= pd.to_datetime(df.YEAR*1000000+df.M*10000+df.D*100+df.H,format='%Y%m%d%H')
     df['tp_corr_nora10'] = Tp_correction(df.TP.values)
     return df
+
+def wind_correction_nora10(df,var='W10'):
+    
+    U15=15
+    p=0.1
+    df.loc[df[var] > U15, var]=df.loc[df[var] > U15, var] + p*(df.loc[df[var] > U15, var]-U15)
+    
+    return df 
+
+
+def Weibull_method_of_moment(X):
+    X=X+0.0001; 
+    n=len(X);
+    m1 = np.mean(X);
+    cm1=np.mean((X-np.mean(X))**1);
+    m2 = np.var(X);
+    cm2=np.mean((X-np.mean(X))**2);
+    m3 = stats.skew(X);
+    cm3 = np.mean((X-np.mean(X))**3);
+    
+    from scipy.special import gamma
+    
+    def m1fun(a,b,c):
+        return a+b*gamma(1+1/c)
+        
+    def cm2fun(b,c):
+        return b**2*(gamma(1+2/c)-gamma(1+1/c)**2)
+        
+    def cm3fun(b,c):
+        return b**3*(gamma(1+3/c)-3*gamma(1+1/c)*gamma(1+2/c)+2*gamma(1+1/c)**3)
+    
+    def cfun(c):
+        return abs(np.sqrt(cm3fun(1,c)**2/cm2fun(1,c)**3)-np.sqrt(cm3**2/cm2**3))
+    
+    from scipy import optimize
+    cHat = optimize.fminbound(cfun, -2, 5) # shape 
+    
+    
+    def bfun(b):
+        return abs(cm2fun(b,cHat)-cm2)
+    bHat = optimize.fminbound(bfun,-5,30) # scale 
+    
+    def afun(a):
+        return abs(m1fun(a,bHat,cHat)-m1)
+    aHat = optimize.fminbound(afun,-5,30) # location
+    
+    return cHat, aHat, bHat # shape, location, scale 
