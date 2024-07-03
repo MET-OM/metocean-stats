37f237ffb69a5afa28e8ea0a9143787b791bb2b4
KonstantinChri <kchrist87@gmail.com>
2024-07-03 17:35:01 +0300

    update

--- metocean_stats/tables/extreme.py
+++ metocean_stats/tables/extreme.py
@@ -342,9 +342,6 @@ def table_wave_induced_current(ds, var_hs,var_tp,max_hs= 20, depth=200,ref_depth
 
 
 def table_profile_return_values(data,var=['W10','W50','W80','W100','W150'], heights=[10, 50, 80, 100, 150], period=[1, 10, 100, 10000], file_out='RVE_wind_profile.csv'):
-   
-    import pandas as pd
-    
     df_wind_profile = pd.DataFrame()
     df_wind_profile['Height above MSL']=heights
     for p in period:
