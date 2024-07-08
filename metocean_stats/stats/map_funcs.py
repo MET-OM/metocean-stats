9e1dd0aab3a5672d834d61c2b31557a311a4ece9
KonstantinChri <kchrist87@gmail.com>
2024-07-08 18:22:45 +0300

    update

--- metocean_stats/stats/map_funcs.py
+++ metocean_stats/stats/map_funcs.py
@@ -3,12 +3,18 @@ import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import xarray as xr
 
-def plot_point_on_map(lon_list, lat_list):
+def plot_point_on_map(lon, lat, label):
+    lon_list = lon
+    lat_list = lat
+    label_list = label
+
     # Ensure lon_list and lat_list are iterable (lists or arrays)
     if not isinstance(lon_list, (list, tuple)):
         lon_list = [lon_list]
     if not isinstance(lat_list, (list, tuple)):
         lat_list = [lat_list]
+    if not isinstance(label_list, (list, tuple)):
+        label_list = [label_list]
 
     # Create the plot with Mollweide projection
     fig = plt.figure(figsize=(10, 8))
@@ -20,14 +26,14 @@ def plot_point_on_map(lon_list, lat_list):
     ax.add_feature(coast)
 
     # Plot the points with automatic labels
-    for i, (lon, lat) in enumerate(zip(lon_list, lat_list)):
-        ax.plot(lon, lat, marker='o', markersize=8, label=f'{lon},{lat}', transform=ccrs.PlateCarree())
+    for i, (lon, lat,label) in enumerate(zip(lon_list, lat_list,label_list)):
+        ax.plot(lon, lat, marker='o', markersize=8, label=f'{label}', transform=ccrs.PlateCarree())
 
     # Calculate zoom-in extent
-    lat_min = max(min(lat_list) - 10, -90)  # Ensure latitude does not go below -90
-    lat_max = min(max(lat_list) + 10, 90)   # Ensure latitude does not go above 90
-    lon_min = max(min(lon_list) - 15, -180) # Ensure longitude does not go below -180
-    lon_max = min(max(lon_list) + 15, 180)  # Ensure longitude does not go above 180
+    lat_min = max(min(lat_list) - 5, -90)  # Ensure latitude does not go below -90
+    lat_max = min(max(lat_list) + 5, 90)   # Ensure latitude does not go above 90
+    lon_min = max(min(lon_list) - 10, -180) # Ensure longitude does not go below -180
+    lon_max = min(max(lon_list) + 10, 180)  # Ensure longitude does not go above 180
 
     # Set extent of the map
     ax.set_extent([lon_min, lon_max, lat_min, lat_max], crs=ccrs.PlateCarree())
