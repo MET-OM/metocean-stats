c846132dd5297a7568ef59712310abf3f0e1e767
Konstantinos Christakos <67804784+KonstantinChri@users.noreply.github.com>
2024-08-07 13:34:31 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -28,8 +28,7 @@ release = 'latest'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['crate.sphinx.csv']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
