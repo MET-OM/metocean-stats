5ec4d7eac8c6060f774768eef64dc5e688c1e1b5
Konstantinos Christakos <67804784+KonstantinChri@users.noreply.github.com>
2024-10-14 00:05:01 +0200

    Update example_report_AI.py

--- examples/example_report_AI.py
+++ examples/example_report_AI.py
@@ -8,9 +8,10 @@ warnings.filterwarnings("ignore")
 import google.generativeai as genai
 import os
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
-model = genai.GenerativeModel(model_name="gemini-1.5-flash")
+#model = genai.GenerativeModel(model_name="gemini-1.5-flash")
+#model = genai.GenerativeModel(model_name="gemini-1.5-flash-exp-0827")
+model = genai.GenerativeModel(model_name='gemini-1.5-flash-latest')
 
-df = readNora10File('../tests/data/NORA_test.txt') 
 
 folder = Path(__file__).parent / 'output' / 'reports'
 if not folder.exists():
@@ -39,21 +40,22 @@ maps.plot_points_on_map(lon=[3.35], lat=[60.40],label=['NORA3'],bathymetry='NORA
 doc.add_heading('Figure 1: The figure shows the NORA3 grid points selected for the analysis', level=2)
 doc.add_picture(output_file, width=Inches(5))
 
-output_file=str(folder / 'wave_100yrs.png')
-maps.plot_extreme_wave_map(return_period=100, product='NORA3', title='100-yr return values Hs (NORA3)', set_extent = [0,30,52,73],output_file=output_file)
-doc.add_heading('Figure 2: 100-year return period for wind speed at 10 m and 100 m in the Nordics based on NORA3 (period: 1991-2020) using Generalized Pareto distribution', level=2)
-doc.add_picture(output_file, width=Inches(5))
+#output_file=str(folder / 'wave_100yrs.png')
+#maps.plot_extreme_wave_map(return_period=100, product='NORA3', title='100-yr return values Hs (NORA3)', set_extent = [0,30,52,73],output_file=output_file)
+#doc.add_heading('Figure 2: 100-year return period for wind speed at 10 m and 100 m in the Nordics based on NORA3 (period: 1991-2020) using Generalized Pareto distribution', level=2)
+#doc.add_picture(output_file, width=Inches(5))
 
-output_file=str(folder / 'wind_100yrs.png')
-maps.plot_extreme_wind_map(return_period=100, product='NORA3',z=10, title='100-yr return values Wind at 100 m (NORA3)', set_extent = [0,30,52,73], output_file=output_file)
-doc.add_heading('Figure 3: 100-year return period for significant wave height in the Nordics based on NORA3 (period: 1991-2020) using Gumbel distribution', level=2)
-doc.add_picture(output_file, width=Inches(5))
+#output_file=str(folder / 'wind_100yrs.png')
+#maps.plot_extreme_wind_map(return_period=100, product='NORA3',z=10, title='100-yr return values Wind at 100 m (NORA3)', set_extent = [0,30,52,73], output_file=output_file)
+#doc.add_heading('Figure 3: 100-year return period for significant wave height in the Nordics based on NORA3 (period: 1991-2020) using Gumbel distribution', level=2)
+#doc.add_picture(output_file, width=Inches(5))
 
 
 
 # Add the first table
 df = tables.table_monthly_non_exceedance(ds,var= 'W10',step_var=2,output_file=None)
-response = model.generate_content("Write one sentence caption to the table:"+str(df))
+print(df)
+response = model.generate_content("Write caption for the following table:"+str(df))
 #doc.add_heading('Table 1: Monthly non-exceedance', level=2)
 doc.add_heading('Table 1:'+response.text, level=2)
 table1 = doc.add_table(rows=df.shape[0] + 1, cols=df.shape[1])
