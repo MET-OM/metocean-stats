05cf9ab74d55f72006c75d075231805001f0669d
theaje <tjellevold@gmail.com>
2025-02-19 10:29:23 +0100

    Added taylor diagram -def taylor_diagram() to >plots>extreme.py -how to run on >examples>example_NORA_full.py

--- examples/example_NORA_full.py
+++ examples/example_NORA_full.py
@@ -10,6 +10,25 @@ folder = Path(__file__).parent / 'output' / 'NORA'
 if not folder.exists():
     folder.mkdir(parents=True)
 
+###########
+# Veritication:
+#New code:
+#Option 1:
+#var_ref = ['HS','HS.2','HS.1','W10']
+#var_comp = ['TP']
+
+#Option 2:
+#var_ref = ['HS']
+#var_comp = ['TP','W10','HS.2']
+
+#Option 3:
+var_ref = ['HS','HS.2','HS.1']
+var_comp = ['TP','W10','HS.2']
+
+plots.taylor_diagram(df,var_ref,var_comp,norm_std=True,output_file=folder /'Testing_Taylor'/ 'Taylor_diagram.png')
+#############
+
+
 # Map:
 maps.plot_points_on_map(lon=[3.35,3.10], lat=[60.40,60.90],label=['NORA3','NORKYST800'],bathymetry='NORA3',output_file='map.png')
 maps.plot_extreme_wave_map(return_period=100, product='NORA3', title='100-yr return values Hs (NORA3)', set_extent = [0,30,52,73],output_file='wave_100yrs.png')
