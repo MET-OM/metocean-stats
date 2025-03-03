9bcf589899a39d6a1ea3db9764fa213803ff48c4
efvik <erikrf@protonmail.com>
2025-03-03 15:05:41 +0100

    put new environmental contours in separate module

--- metocean_stats/plots/contours.py
+++ metocean_stats/plots/contours.py
@@ -1,6 +1,7 @@
-import virocon
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import virocon
 
 ########################
 # Dependence functions
@@ -132,7 +133,7 @@ def description_windsea_hs_tp():
 
     dist_description_hs = {
         "distribution": virocon.WeibullDistribution(),
-        "intervals": virocon.NumberOfIntervalsSlicer(16),
+        "intervals": virocon.NumberOfIntervalsSlicer(13),
     }
     dist_description_tp = {
         "distribution": virocon.ExponentiatedWeibullDistribution(f_delta=20),
@@ -156,6 +157,8 @@ def description_windsea_hs_tp():
     }
 
     return dist_descriptions, fit_descriptions, semantics
+
+
 
 #############
 # Procedures
@@ -233,14 +236,16 @@ def fit_joint_model(
     # Load preset
     if preset == 'OMAE_hs_tp':
         dist_descriptions, fit_descriptions, semantics = description_OMAE_hs_tp()
-    if preset == 'DNVGL_hs_tp':
+    elif preset == 'DNVGL_hs_tp':
         dist_descriptions, fit_descriptions, semantics = description_DNVGL_hs_tp()
-    if preset == 'OMAE_U_hs':
+    elif preset == 'OMAE_U_hs':
         dist_descriptions, fit_descriptions, semantics = description_OMAE_U_hs()
-    if preset == 'DNVGL_hs_U':
+    elif preset == 'DNVGL_hs_U':
         dist_descriptions, fit_descriptions, semantics = description_DNVGL_hs_U()
-    if preset == 'windsea_hs_tp':
+    elif preset == 'windsea_hs_tp':
         dist_descriptions, fit_descriptions, semantics = description_windsea_hs_tp()
+    else:
+        raise ValueError(f'Preset {preset} not found. See docstring for available presets.')
 
     # Fit and return model
     model = virocon.GlobalHierarchicalModel(dist_descriptions)
@@ -248,3 +253,116 @@ def fit_joint_model(
     return model, semantics
 
 
+
+def plot_environmental_contours(
+        data:pd.DataFrame,
+        var1:str,
+        var2:str,
+        config:str = 'DNVGL_hs_tp',
+        save_path = 'joint_distribution',
+        return_periods = [1,10,100],
+        sea_state_duration:float = 1,
+        plot_marginal_quantiles = True,
+        plot_interval_histograms = True,
+        plot_dependency_functions = True,
+        plot_isodensity = True
+        ):
+    
+    """
+    Fit 2D joint probability model and produce associated plots.
+    This only offers some default configurations.
+    For more detailed configuration options, the virocon package should be consulted directly.
+    This module can then be used as an example.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        The dataframe containing var_hs and var_T as columns.
+    var1 : str
+        The wave height column.
+    var2 : str
+        The wave period column.
+    config : str
+        Preset configurations for different cases:
+
+            - DNVGL_hs_tp: weibull and lognormal
+            - OMAE_hs_tp: exp. weibull and lognormal
+            - DNVGL_hs_U: weibull and weibull
+            - OMAE_U_hs: exp. weibull and exp. weibull
+            - windsea_hs_tp: weibull and exp. weibull, 
+                works well for wind sea
+
+    """
+
+    if type(var1) == int:
+        var1 = data.columns[var1]
+    if type(var2) == int:
+        var2 = data.columns[var2]
+    data = data[[var1,var2]]
+
+    model, semantics = fit_joint_model(data,preset=config)
+
+    if config in ['DNVGL_hs_tp','OMAE_hs_tp','DNVGL_hs_U','windsea_hs_tp']:
+        swap_axes = True
+    else:
+        swap_axes = False
+
+    figures = []
+
+    if return_periods:
+        fig,ax = plt.subplots()
+
+        for i,T_return in enumerate(return_periods):
+            alpha = virocon.calculate_alpha(sea_state_duration,T_return)
+            contour = virocon.IFORMContour(model, alpha)
+            
+            if i == 0: # only plot the data once
+                virocon.plot_2D_contour(contour, data, semantics=semantics, swap_axis=swap_axes, ax=ax)
+            else:
+                virocon.plot_2D_contour(contour, semantics=semantics, swap_axis=swap_axes, ax=ax)
+        
+        figures.append(fig)
+        if save_path:
+            fig.savefig(save_path+'_contours.png')
+
+    if plot_marginal_quantiles:
+        try:
+            fig,ax = plt.subplots(1,2)
+            virocon.plot_marginal_quantiles(model,data,semantics,axes=ax)
+            figures.append(fig)
+            if save_path:
+                fig.savefig(save_path+'_quantiles.png')
+        except Exception as e:
+            print(f"Warning - could not create quantiles plots \n: {e}")
+
+    if plot_interval_histograms:
+        try:
+            fig,ax = virocon.plot_histograms_of_interval_distributions(model,data,semantics)
+            for i,f in enumerate(fig):
+                figures.append(f)
+                if save_path:
+                    f.savefig(save_path+'_histograms.png')
+        except Exception as e:
+            print(f"Warning - could not create histogram plots \n: {e}")
+
+    if plot_dependency_functions:
+        try:
+            fig,ax = plt.subplots(1,2)
+            virocon.plot_dependence_functions(model,semantics,axes=ax)
+            figures.append(fig)
+            if save_path:
+                fig.savefig(save_path+'_dependences.png')
+        except Exception as e:
+            print(f"Warning - could not create dependence plots \n: {e}")
+    
+    if plot_isodensity:
+        try:
+            fig,ax = plt.subplots()
+            virocon.plot_2D_isodensity(model,data,semantics,swap_axis=swap_axes, ax=ax)
+            figures.append(fig)
+            if save_path:
+                fig.savefig(save_path+'_isodensity.png')
+        except Exception as e:
+            print(f"Warning - could not create isodensity plot \n: {e}")
+
+    return figures
