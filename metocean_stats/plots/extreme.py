7b0d58b414421158461f8cafc4e1b7669de695d1
Clio Michel <141409297+clio-met@users.noreply.github.com>
2025-03-21 15:44:31 +0100

    Modify size figure CCA profiles

--- metocean_stats/plots/extreme.py
+++ metocean_stats/plots/extreme.py
@@ -1068,7 +1068,7 @@ def plot_cca_profiles(data,var='current_speed_',month=None,percentile=None,retur
     n_lines = len(lev)
     cmap = mpl.colormaps['jet_r']
     colors = cmap(np.linspace(0, 1, n_lines))
-    fig, ax = plt.subplots(figsize=(9,7))
+    fig, ax = plt.subplots(figsize=(9,10))
     for d in range(len(lev)):
         ax.plot(lev,cca[:,d],color=colors[d],label=str(int(lev[d]))+' m',linewidth=2)
     ax.plot(lev,woca,label='Worst case',color='k',linewidth=3.5)
