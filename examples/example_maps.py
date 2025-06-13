111300ed288ec32447789e510212b6bcedfb194a
SChristineP <S.Christine.P@outlook.com>
2025-06-13 09:51:47 +0200

    Edit description

--- examples/example_maps.py
+++ examples/example_maps.py
@@ -7,7 +7,7 @@ folder = Path(__file__).parent / 'maps'
 if not folder.exists():
     folder.mkdir(parents=True)
 
-# Map:
+# The following functions will illustrate statistics in a map format:
 maps.plot_points_on_map(lon=[3.35,3.10], lat=[60.40,60.90],label=['NORA3','NORKYST800'],bathymetry='NORA3',output_file= folder /'map.png')
 maps.plot_extreme_wave_map(return_period=100, product='NORA3', title='100-yr return values Hs (NORA3)', set_extent = [0,30,52,73],output_file= folder / 'wave_100yrs.png')
 maps.plot_extreme_wind_map(return_period=100, product='NORA3',z=100, title='100-yr return values Wind at 100 m (NORA3)', set_extent = [0,30,52,73], output_file=folder / 'wind_100yrs_100m.png')
